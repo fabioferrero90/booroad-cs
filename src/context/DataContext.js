a1f0446ce04b08616f265d3219ab8cd383cc2ec3
fabioferrero90 <fabio.ff90@gmail.com>
2025-02-14 10:05:58 +0100

    first commit

---
+++ src/context/DataContext.js
@@ -0,0 +1,20 @@
+import { useContext, createContext } from 'react';
+const DataContext = createContext();
+import db from '../data/db.json';
+
+const DataProvider = ({ children, }) => {
+
+  const data = JSON.parse(JSON.stringify(db));
+
+  console.log(data.users)
+
+
+  return (
+    <DataContext.Provider value={value}>
+      {children}
+    </DataContext.Provider>
+  )
+};
+
+const useDataContext = () => useContext(DataContext);
+export { useDataContext, DataProvider };
