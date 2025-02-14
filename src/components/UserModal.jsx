187ea5e26a886e569c993be36d4f4d028a5f9159
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-14 15:18:15 +0100

    feat: update UserModal styling for improved user experience

--- src/components/UserModal.jsx
+++ src/components/UserModal.jsx
@@ -7,78 +7,54 @@ const UserModal = ({ closeModal }) => {
         className="fixed inset-0 bg-black opacity-50"
         onClick={closeModal}
       ></div>
-      <div className="bg-white rounded-lg p-8 z-10 max-w-md w-full">
-        <h2 className="text-xl mb-4">Aggiungi Accompagnatore</h2>
+      <div className="bg-blue-600 rounded-lg p-8 z-10 max-w-md w-full">
+        <h2 className="text-xl mb-4 text-white">Aggiungi Accompagnatore</h2>
         <form className="space-y-4">
           <div>
-            <label
-              htmlFor="nome"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Nome
-            </label>
             <input
               type="text"
               id="nome"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
               placeholder="Nome"
               required
             />
           </div>
           <div>
-            <label
-              htmlFor="cognome"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Cognome
-            </label>
             <input
               type="text"
               id="cognome"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
               placeholder="Cognome"
               required
             />
           </div>
           <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Email
-            </label>
             <input
               type="email"
               id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
               placeholder="Email"
               required
             />
           </div>
           <div>
-            <label
-              htmlFor="telefono"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Telefono
-            </label>
             <input
               type="tel"
               id="telefono"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
               placeholder="Telefono"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="w-full text-white bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Aggiungi
           </button>
         </form>
         <button
-          className="mt-4 text-gray-500 hover:text-gray-700"
+          className="mt-4 text-white hover:text-gray-700 "
           onClick={closeModal}
         >
           Chiudi
