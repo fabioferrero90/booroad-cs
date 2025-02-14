9491e5e99b50f5347a7710d6751e310899ebb433
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-14 14:58:29 +0100

    feat: enhance Users page layout with improved styling and user addition section

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,27 +1,42 @@
 const Users = () => {
   return (
-    //gray bar
-    <div className="container mx-auto p-4">
-      <div className="border-b rounded-lg mb-4 bg-gray-500 m-2.5 p-1">
-        <input
-          type="text"
-          placeholder="Search"
-          className="p-2 rounded border border-gray-300 bg-gray-200"
-        />
+    <>
+      {/* gray bar */}
+      <div className="container mx-auto p-4">
+        <div className="rounded-lg mb-4 bg-neutral-500 m-2.5 p-1">
+          <input
+            type="text"
+            placeholder="Search"
+            className="p-2 rounded border border-gray-300 bg-gray-200"
+          />
 
-        <div className="justify-end flex">
-          <select className="border border-gray-300 rounded p-2 m-3.5 flex  rounded-lg bg-gray-200">
-            <option value="option1">DALLA A ALLA Z</option>
-            <option value="option2">DALLA Z ALLA A</option>
-          </select>
+          <div className="justify-end flex">
+            <select className="border border-gray-300 rounded p-2 m-3.5 flex rounded-lg bg-gray-200">
+              <option value="option1">DALLA A ALLA Z</option>
+              <option value="option2">DALLA Z ALLA A</option>
+            </select>
 
-          <select className="border border-gray-300 rounded p-2 m-3.5  rounded-lg bg-gray-200">
-            <option value="option1">IN VIAGGIO AL</option>
-            <option value="option2">IN VIAGGIO DAL</option>
-          </select>
+            <select className="border border-gray-300 rounded p-2 m-3.5 rounded-lg bg-gray-200">
+              <option value="option1">IN VIAGGIO AL</option>
+              <option value="option2">IN VIAGGIO DAL</option>
+            </select>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* users */}
+      <div className="container mx-auto p-4 max-w-350">
+        <div className="rounded-lg bg-gray-700 p-4 flex justify-start my-3">
+          <h2 className="text-white">FRANCO ROSSI</h2>
+        </div>
+        <div className="rounded-lg bg-gray-700 p-4 flex justify-start my-3">
+          <h2 className="text-white">FRANCO ROSSI</h2>
+        </div>
+        <div className="rounded-lg bg-neutral-400 p-4 flex justify-center my-3">
+          <h2 className="text-white">AGGIUNGI ACCOMPAGNATORE</h2>
+        </div>
+      </div>
+    </>
   );
 };
 
