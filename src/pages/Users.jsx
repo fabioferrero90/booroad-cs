9396d159b52f3147f829d3c9316de1e9b2d5bc59
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-14 13:18:03 +0100

    feat: update Header component styling and enhance Users page with search and filter options

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,28 @@
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    //gray bar
+    <div className="container mx-auto p-4">
+      <div className="border-b rounded-lg mb-4 bg-gray-500 m-2.5 p-1">
+        <input
+          type="text"
+          placeholder="Search"
+          className="p-2 rounded border border-gray-300 bg-gray-200"
+        />
+
+        <div className="justify-end flex">
+          <select className="border border-gray-300 rounded p-2 m-3.5 flex  rounded-lg bg-gray-200">
+            <option value="option1">DALLA A ALLA Z</option>
+            <option value="option2">DALLA Z ALLA A</option>
+          </select>
+
+          <select className="border border-gray-300 rounded p-2 m-3.5  rounded-lg bg-gray-200">
+            <option value="option1">IN VIAGGIO AL</option>
+            <option value="option2">IN VIAGGIO DAL</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
