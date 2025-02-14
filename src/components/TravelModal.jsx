b96f1b1eb15f52dab5043848df1810215c2dd349
galdoben <galdobenedetta@gmail.com>
2025-02-14 16:33:10 +0100

    add: travel-modal

---
+++ src/components/TravelModal.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const TravelModal = ({ closeModal }) => {
+    return (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div
+                className="fixed inset-0 bg-black opacity-50"
+                onClick={closeModal}
+            ></div>
+            <div className="bg-blue-600 rounded-lg p-8 z-10 max-w-md w-full">
+                <h2 className="text-xl mb-4 text-white font-bold">
+                    AGGIUNGI NUOVO VIAGGIO
+                </h2>
+                <form className="space-y-4">
+                    <div>
+                        <input
+                            type="text"
+                            id="destination"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
+                            placeholder="Destinazione"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            id="start-date"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
+                            placeholder="Data di partenza"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            id="end-date"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
+                            placeholder="Data di ritorno"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            id="itinerary"
+                            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black"
+                            placeholder="Itinerario"
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full text-white bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Aggiungi
+                    </button>
+                </form>
+                <button
+                    className="mt-4 text-white hover:text-gray-700 "
+                    onClick={closeModal}
+                >
+                    Chiudi
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default TravelModal;
--- src/pages/Travels.jsx
+++ src/pages/Travels.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { useState } from "react";
 import { destinations, travels } from "../data/db.json";
+import TravelModal from "../components/TravelModal";
 
 const Travels = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+
   const toggleDropdown = () => setIsOpen(!isOpen);
   return (
     <div className="p-4">
-      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" aria-expanded={isOpen ? 'true' : 'false'} onClick={toggleDropdown}>Previous Trips  <svg class="w-2.5 h-2.5 ms-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-      </svg>
-      </button>
-      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" aria-expanded={isOpen ? 'true' : 'false'} onClick={toggleDropdown}>Trips Coming Soon  <svg class="w-2.5 h-2.5 ms-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-      </svg>
-      </button>
+      <div className="rounded-lg mb-4 bg-neutral-500 m-2.5 p-4 flex justify-between items-center">
+        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" aria-expanded={isOpen ? 'true' : 'false'} >Viaggi Precedenti <svg class="w-2.5 h-2.5 ms-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+        </svg>
+        </button>
+      </div>
+      <div className="rounded-lg mb-4 bg-neutral-500 m-2.5 p-4 flex justify-between items-center">
+        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" aria-expanded={isOpen ? 'true' : 'false'} onClick={toggleDropdown}>Viaggi in Arrivo<svg class="w-2.5 h-2.5 ms-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+        </svg>
+        </button>
+      </div>
 
 
       {isOpen && (
@@ -33,10 +50,10 @@ const Travels = () => {
                     <a href="#">
                       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{destination.destination_name}</h5>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{travel.start_date}{travel.end_date}</p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{new Date(travel.start_date).toLocaleDateString()}{"  "}{new Date(travel.end_date).toLocaleDateString()}</p>
                     <p className="mb-4 text-sm text-blue-300">{travel.abstract}</p>
                     <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                      Read more
+                      Vedi Dettagli
                       <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                       </svg>
@@ -50,10 +67,15 @@ const Travels = () => {
         </div>
 
       )}
-      <div className="p-6 bg-violet-500 m-4 text-center ">
-        <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add New Trip</button>
+
+      <div className="rounded-lg bg-neutral-400 p-4 flex justify-center my-3">
+        <h2 className="text-white cursor-pointer" onClick={openModal}>
+          + AGGIUNGI NUOVO VIAGGIO
+        </h2>
       </div>
+      {showModal && <TravelModal closeModal={closeModal} />}
     </div>
+
   )
 }
 
