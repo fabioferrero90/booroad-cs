04991fbbfb706fbb7dde7a9feeac700c2c06a53e
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-02-14 16:31:57 +0100

    add: customerDetails card

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,4 +1,5 @@
 import CustomerDetails from "./CustomerDetails";
+
 const contacts = [
   { name: 'FRANCO ROSSETTI' },
   { name: 'GIACOMO NICOLINI' },
@@ -32,6 +33,7 @@ const Customers = () => {
           <div key={index} className="flex items-center justify-between p-4 bg-gray-700 rounded mb-2">
             <span>{contact.name}</span>
             <div className="flex space-x-4">
+              {/* mail */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -46,6 +48,7 @@ const Customers = () => {
                   d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
                 />
               </svg>
+              {/* phone */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -60,6 +63,7 @@ const Customers = () => {
                   d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
                 />
               </svg>
+              {/* contact */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -74,6 +78,7 @@ const Customers = () => {
                   d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
                 />
               </svg>
+              {/* message */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
