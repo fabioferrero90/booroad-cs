ff730f7cd01bcc5e2f93df14eefb647689fc33cb
galdoben <galdobenedetta@gmail.com>
2025-02-14 15:53:18 +0100

    fix: page-travels

--- src/pages/Travels.jsx
+++ src/pages/Travels.jsx
@@ -1,5 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import { destinations, travels } from "../data/db.json";
+
 const Travels = () => {
-  return <div>Travels</div>;
-};
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  return (
+    <div className="p-4">
+      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" aria-expanded={isOpen ? 'true' : 'false'} onClick={toggleDropdown}>Previous Trips  <svg class="w-2.5 h-2.5 ms-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+      </svg>
+      </button>
+      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" aria-expanded={isOpen ? 'true' : 'false'} onClick={toggleDropdown}>Trips Coming Soon  <svg class="w-2.5 h-2.5 ms-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+      </svg>
+      </button>
+
+
+      {isOpen && (
+        <div className="max-w-400 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+            {travels.map((travel) => {
+              const destination = destinations.find(
+                (dest) => dest.destination_id === travel.destination_id
+              );
+
+              return (
+                <div key={travel.id_viaggio} className="max-w-xs bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                  <div className="bg-cover h-48 rounded-t-lg" style={{ backgroundImage: `url(${destination.image})` }}>
+                  </div>
+                  <div className="p-4">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{destination.destination_name}</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{travel.start_date}{travel.end_date}</p>
+                    <p className="mb-4 text-sm text-blue-300">{travel.abstract}</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Read more
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+
+              );
+            })}
+          </div>
+        </div>
+
+      )}
+      <div className="p-6 bg-violet-500 m-4 text-center ">
+        <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add New Trip</button>
+      </div>
+    </div>
+  )
+}
 
 export default Travels;
